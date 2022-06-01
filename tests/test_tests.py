fa0ba46638ab847645035f0c531775164845a1fb
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-06-01 13:23:22 +0300

    Add test

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -17,3 +17,20 @@ def test_expiration_day_today_not_outdated(monkeypatch):
     assert test_result.value == 1, (
         "Product with expiration date equals today is not outdated."
     )
+
+
+def test_expiration_day_yesterday_outdated(monkeypatch):
+    def expiration_day_yesterday_not_outdated(products: list):
+        import datetime
+        return [product["name"] for product in products
+                if (product["expiration_date"] <
+                    datetime.date.today() - datetime.timedelta(days=1))]
+
+    monkeypatch.setattr(
+        main, "outdated_products", expiration_day_yesterday_not_outdated
+    )
+
+    test_result = pytest.main(["app/test_main.py"])
+    assert test_result.value == 1, (
+        "Product with expiration date equals yesterday is outdated."
+    )
