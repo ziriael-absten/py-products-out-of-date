3be5cf962647a4d737bd80e09f26c3c4e15fb05d
Yaroslav Havryliv <yaroslav.h@mate.academy>
2022-10-05 17:03:18 +0300

    annotations added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import datetime
 
 
-def outdated_products(products: list):
+def outdated_products(products: list) -> list:
     return [product["name"] for product in products
             if product["expiration_date"] < datetime.date.today()]
